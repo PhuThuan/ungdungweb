50f5419cf5b3370fc46a530841020f168f80eeb4
PhuThuan <phuthuan1910@gmail.com>
2023-02-17 19:55:48 +0700

    "Cai dat xu ly loi"

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    res.send({ message: 'create handler' });
+};
+
+exports.findALl = (req, res) => {
+    res.send({ message: 'findALl handler' });
+};
+
+exports.findOne = (req, res) => {
+    res.send({ message: 'findOne handler' });
+};
+
+exports.update = (req, res) => {
+    res.send({ message: 'update handler' });
+};
+
+exports.delete = (req, res) => {
+    res.send({ message: 'delete handler' });
+};
+
+exports.deleteALl = (req, res) => {
+    res.send({ message: 'deleteALl handler' });
+};
+
+exports.findALlFavorite = (req, res) => {
+    res.send({ message: 'findALlFavorite handler' });
+};
